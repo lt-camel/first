a1a516268f1a6ea70aa52a8b4a8d512ae05510ce
Liu tao <54888639+taot-Liu@users.noreply.github.com>
2019-09-16 21:22:18 +0800

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,13 @@
+package Learning;
+
+public class Hello {
+    public static void main(String []args){
+        int x = 10 ;
+        int y = 4 ;
+        int z = 10>5?10:5 ;
+        int i = 19&20 ;
+        System.out.println(x / y);
+        System.out.println(z);
+        System.out.println(i);
+    }
+}
