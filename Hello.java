9efaedb218b8d419da806c64c6376e5c4c4eacc0
Liu tao <54888639+taot-Liu@users.noreply.github.com>
2019-09-16 21:24:17 +0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,12 +2,6 @@ package Learning;
 
 public class Hello {
     public static void main(String []args){
-        int x = 10 ;
-        int y = 4 ;
-        int z = 10>5?10:5 ;
-        int i = 19&20 ;
-        System.out.println(x / y);
-        System.out.println(z);
-        System.out.println(i);
+        System.out.println("Hello World!");
     }
 }
